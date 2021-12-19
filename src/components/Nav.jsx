68ca35ec83b34dc2fdc58235577e836f4e053c0c
Debatosh Pal Majumder <36793454+debatoshpm@users.noreply.github.com>
2021-12-19 16:20:41 +0530

    nav fixed

--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -20,44 +20,45 @@ const Nav = () => {
             <span className="navbar-toggler-icon"></span>
           </button>
 
-        <div className="collapse navbar-collapse" id="navmenu">
-          <ul className="navbar-nav ms-auto">
-            <li className="nav-item">
-              <Link to="/" className="nav-link">
-                Home
-              </Link>
-            </li>
-            <li className="nav-item">
-              <a href="/#" className="nav-link">
-                About Us
-              </a>
-            </li>
-            <li className="nav-item">
-              <a href="/#" className="nav-link">
-                Natural Dyes
-              </a>
-            </li>
-            <li className="nav-item">
-              <a href="/#" className="nav-link">
-                Certificates
-              </a>
-            </li>
-            <li className="nav-item">
-              <a href="/#" className="nav-link">
-                Blog
-              </a>
-            </li>
-            <li className="nav-item">
-              <a href="/#" className="nav-link">
-                Career
-              </a>
-            </li>
-            <li className="nav-item">
-              <Link to="/contact" className="nav-link">
-                Contact Us
-              </Link>
-            </li>
-          </ul>
+          <div className="collapse navbar-collapse" id="navmenu">
+            <ul className="navbar-nav ms-auto">
+              <li className="nav-item">
+                <Link to="/" className="nav-link">
+                  Home
+                </Link>
+              </li>
+              <li className="nav-item">
+                <a href="/#" className="nav-link">
+                  About Us
+                </a>
+              </li>
+              <li className="nav-item">
+                <a href="/#" className="nav-link">
+                  Natural Dyes
+                </a>
+              </li>
+              <li className="nav-item">
+                <a href="/#" className="nav-link">
+                  Certificates
+                </a>
+              </li>
+              <li className="nav-item">
+                <a href="/#" className="nav-link">
+                  Blog
+                </a>
+              </li>
+              <li className="nav-item">
+                <a href="/#" className="nav-link">
+                  Career
+                </a>
+              </li>
+              <li className="nav-item">
+                <Link to="/contact" className="nav-link">
+                  Contact Us
+                </Link>
+              </li>
+            </ul>
+          </div>
         </div>
       </div>
     </nav>
