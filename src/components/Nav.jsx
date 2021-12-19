dbafba8e34bb9caf50d22f0209cca7080ae6cc34
Soham Chakraborty <soham.chakrabortyofficial@gmail.com>
2021-12-19 15:55:34 +0530

    navbar changed

--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -4,58 +4,60 @@ import logo from "../images/Logo.png";
 const Nav = () => {
   return (
     <nav className="navbar navbar-expand-lg bg-light navbar-light fixed-top p-0">
-      <div className="container">
-        <a href="/#" className="navbar-brand">
-          <img src={logo} className="nav-logo-icon" alt="icon-logo" />
-        </a>
+      <div className="navContainer_outside">
+        <div className="navContainer">
+          <a href="/#" className="navbar-brand">
+            <img src={logo} className="nav-logo-icon" alt="icon-logo" />
+          </a>
 
-        <button
-          className="navbar-toggler"
-          type="button"
-          data-bs-toggle="collapse"
-          data-bs-target="#navmenu"
-        >
-          <span className="navbar-toggler-icon"></span>
-        </button>
+          <button
+            className="navbar-toggler"
+            type="button"
+            data-bs-toggle="collapse"
+            data-bs-target="#navmenu"
+          >
+            <span className="navbar-toggler-icon"></span>
+          </button>
 
-        <div className="collapse navbar-collapse" id="navmenu">
-          <ul className="navbar-nav ms-auto">
-            <li className="nav-item">
-              <a href="/#" className="nav-link">
-                Home
-              </a>
-            </li>
-            <li className="nav-item">
-              <a href="/#" className="nav-link">
-                About Us
-              </a>
-            </li>
-            <li className="nav-item">
-              <a href="/#" className="nav-link">
-                Natural Dyes
-              </a>
-            </li>
-            <li className="nav-item">
-              <a href="/#" className="nav-link">
-                Certificates
-              </a>
-            </li>
-            <li className="nav-item">
-              <a href="/#" className="nav-link">
-                Blog
-              </a>
-            </li>
-            <li className="nav-item">
-              <a href="/#" className="nav-link">
-                Career
-              </a>
-            </li>
-            <li className="nav-item">
-              <a href="/#" className="nav-link">
-                Contact Us
-              </a>
-            </li>
-          </ul>
+          <div className="collapse navbar-collapse" id="navmenu">
+            <ul className="navbar-nav ms-auto">
+              <li className="nav-item">
+                <a href="/#" className="nav-link">
+                  Home
+                </a>
+              </li>
+              <li className="nav-item">
+                <a href="/#" className="nav-link">
+                  About Us
+                </a>
+              </li>
+              <li className="nav-item">
+                <a href="/#" className="nav-link">
+                  Natural Dyes
+                </a>
+              </li>
+              <li className="nav-item">
+                <a href="/#" className="nav-link">
+                  Certificates
+                </a>
+              </li>
+              <li className="nav-item">
+                <a href="/#" className="nav-link">
+                  Blog
+                </a>
+              </li>
+              <li className="nav-item">
+                <a href="/#" className="nav-link">
+                  Career
+                </a>
+              </li>
+              <li className="nav-item">
+                <a href="/#" className="nav-link">
+                  Contact Us
+                </a>
+              </li>
+            </ul>
+          </div>
         </div>
       </div>
     </nav>
